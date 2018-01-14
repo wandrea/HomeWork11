f6fe6dd4ed56c4994c7243f2d82ebccc5ab19a49
wandrea <wagenhofferandrea@gmail.com>
2018-01-14 12:56:37 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrea on 2018.01.13..
+ */
+public class Main {
+    public static void main(String[] args) {
+      //  Util.printYoutubeJson("d:\\HELIXLAB_TANFOLYAM\\JAVA\\projects\\HomeWork11\\src\\main\\resources\\youtube.json");
+       // Util.convertYoutubeJSONToXML("d:\\HELIXLAB_TANFOLYAM\\JAVA\\projects\\HomeWork11\\src\\main\\resources\\youtube.json", "d:\\HELIXLAB_TANFOLYAM\\JAVA\\projects\\HomeWork11\\src\\main\\resources\\youtube.xml");
+    }
+}
